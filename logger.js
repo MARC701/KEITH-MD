108cb72fa8dc0d8c9e0d2a6b5204f76bf605da93
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-07-18 14:02:33 +0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -39,7 +39,7 @@ class KeithLogger {
         const messageType = m.mtype;
         const text = m.text || '';
 
-        console.log(keithPurple.bold(`\t ${BOT_SYMBOL} ${BOT_SYMBOL} ${BOT_SYMBOL} { ᴹᴬᶠᴵᴬ⁻ᴹᴰ } ${BOT_SYMBOL} ${BOT_SYMBOL} ${BOT_SYMBOL}`));
+        console.log(keithPurple.bold(`\t ${BOT_SYMBOL} ${BOT_SYMBOL} ${BOT_SYMBOL} { 𝐌𝐀𝐅𝐈𝐀-𝐌𝐃 } ${BOT_SYMBOL} ${BOT_SYMBOL} ${BOT_SYMBOL}`));
         console.log(keithGold.bold("╔════════════════════════════╗"));
         console.log(keithGold.bold(`║ ${MESSAGE_SYMBOL}   N E W   M E S S A G E   ${MESSAGE_SYMBOL} ║`));
         console.log(keithGold.bold("╚════════════════════════════╝"));
