d09fd0117d80638ccdc38310982437e1ba0c79f5
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-10-30 08:35:10 +0500

    Change default value of status to true

--- database/autolikestatus.js
+++ database/autolikestatus.js
@@ -4,7 +4,7 @@ const { database } = require('../settings');
 const AutoLikeStatusDB = database.define('autolikestatus', {
     status: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false,
+        defaultValue: true,
         allowNull: false
     },
     delay: {
