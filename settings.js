74700139d2f1fb2b8a1cbdb679f92cab440d14d8
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+MARC701@users.noreply.github.com>
2025-10-30 20:20:14 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,31 +10,31 @@ const session = process.env.SESSION || '';
 
 const prefix = process.env.PREFIX || '.';
 
-const author = process.env.OWNER_NAME || 'Keith';
+const author = process.env.OWNER_NAME || '𝐀𝐫𝐬𝐥𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐚𝐫𝐲';
 
-const packname = process.env.PACKNAME || 'keith';
+const packname = process.env.PACKNAME || '𝐌𝐀𝐅𝐈𝐀-𝐓𝐞𝐜𝐡';
 
-const dev = process.env.OWNER_NUMBER || '254748387615';
+const dev = process.env.OWNER_NUMBER || '923111977378';
 
 const DevKeith = dev.split(",");
 
-const antibot = process.env.ANTIBOT || 'true';
+const antibot = process.env.ANTIBOT || 'false';
 
-const botname = process.env.BOTNAME || 'KEITH-MD';
+const botname = process.env.BOTNAME || '𝐌𝐀𝐅𝐈𝐀-𝐌𝐃';
 
-const mode = process.env.MODE || 'public';
+const mode = process.env.MODE || 'private';
 
 const sessionName = "session";
 
-const url = process.env.URL || 'https://files.catbox.moe/mikdi0.jpg';
+const url = process.env.URL || 'https://ik.imagekit.io/Arslan22/IMG-20250725-WA0121.jpg';
 
-const gurl = process.env.GURL || 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47';
+const gurl = process.env.GURL || 'https://whatsapp.com/channel/0029Vat4TFC0QeaoLURbP61u';
 
-const timezone = process.env.TIMEZONE || 'Africa/Nairobi';
+const timezone = process.env.TIMEZONE || 'Asia/Karachi';
 
 //fo mega users only
-const megaEmail = process.env.MEGA_EMAIL || '';
-const megaPassword = process.env.MEGA_PASSWORD || '';
+const megaEmail = process.env.MEGA_EMAIL || 'arslanchaudary701@gmail.com';
+const megaPassword = process.env.MEGA_PASSWORD || 'Ac714744786$';
 
 const { Sequelize } = require('sequelize'); // Ensure Sequelize is imported
 const DATABASE_URL = process.env.DATABASE_URL || './database.db'; // Define DATABASE_URL properly
